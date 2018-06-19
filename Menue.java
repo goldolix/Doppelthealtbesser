456bd8406c5f379bee27c93ec205ab520f055e62
diana1993muesschrewe <38688774+diana1993muesschrewe@users.noreply.github.com>
2018-06-19 18:08:27 +0200

    Add files via upload

--- Menue.java
+++ Menue.java
@@ -1,6 +1,8 @@
-
+/** @author Diana Schumacher **/
 package Spielen;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,11 +10,15 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 
 public class Menue extends JPanel
 {
+	//Versionsnummer
 	private static final long serialVersionUID = 1L;
 
+	//Menü Grundeinstellungen
 	public Menue()
 	{
 		this.setLocation(470, 10);
@@ -20,10 +26,12 @@ public class Menue extends JPanel
 		this.setLayout(null);
 		this.setBorder(BorderFactory.createRaisedBevelBorder());
 		
+		
+		//Methode exit, mit erneuter Abfrage
 		JButton exit = new JButton();
 		exit.setLocation(10, 10);
 		exit.setSize(86, 20);
-		exit.setText("Aufgeben!");
+		exit.setText("Beenden");
 		exit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0)
 			{
@@ -35,5 +43,47 @@ public class Menue extends JPanel
 			}
 		});
 		this.add(exit);
-	}
+		
+		
+		 //Methode start, mit erneuter Abfrage
+				JButton start = new JButton();
+				start.setLocation(10, 60);
+				start.setSize(86, 20);
+				start.setText("Starten");
+				start.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0)
+					{
+						int i = JOptionPane.showConfirmDialog(null,
+								"Möchtest du ein neues Spiel laden?", null,
+								JOptionPane.YES_NO_OPTION);
+						if(i == JOptionPane.YES_OPTION)
+							new PlayingBoard();
+					}
+				});
+				this.add(start);
+	
+	/**				
+	//Methode name, Spielername
+			JButton name = new JButton();
+			exit.setLocation(10, 10);
+			exit.setSize(86, 20);
+			exit.setText("Spieler");
+			exit.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0)
+				{
+					  JTextField tfName = new JTextField("Gib deinen Namen ein!", 1);
+				        Container panel;
+						// Textfeld wird unserem Panel hinzugefügt
+				        panel.add(tfName);
+				 
+				        JButton buttonOK = new JButton("OK");
+				        panel.add(buttonOK);
+				 
+				        gamePanel.add(panel);
+				        gamePanel.setVisible(true);
+				 
+				    };
+			
+		});  **/
+}
 }
